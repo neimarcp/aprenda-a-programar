60854a686826bf2b932df3d2ab75ce1737484632
neimarcp <97646316+neimarcp@users.noreply.github.com>
2022-08-26 17:20:28 -0300

    Update pratica-4.js

--- tarefa-dia-3/pratica-4.js
+++ tarefa-dia-3/pratica-4.js
@@ -1,1 +1,16 @@
 
+
+let numero = 17 //"b": 2, "c": 7, "d": 8, "e": 5, "f": 23, "g": 15, "h": 1500450271}
+let divisor = 2;
+
+while(numero.a%divisor != 0)
+    divisor++;
+
+if(numero.a == divisor){
+    console.log("Número Primo!");
+}else
+{
+    console.log("Número NÃO primo!");
+}
+
+
