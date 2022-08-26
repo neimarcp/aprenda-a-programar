72642e25d74a347dc416351a49abb4fea2385633
neimarcp <neiamrcpereiral@gmail.comr>
2022-08-26 14:53:56 -0300

    tarefa-dia-3

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,13 @@
+let nome = "Neimar"
+let email = 'neimarcpereira@gmail.com'
+let profissão = 'Auditor'
+let idade = '39'
+let lugar = 'EUA'
+console.log("Olá, ",nome,
+"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ", lugar, ". Confirma pra gente alguns dados? A sua idade é ", idade, ", você é ", profissão, " e o seu email é ", email, 
+"? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
+console.log(nome)
+console.log(email)
+console.log(profissão)
+console.log(idade)
+console.log(lugar)
---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,45 @@
+let produtoA_valor2
+let produtoB_valor2
+let produtoC_valor2
+
+let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
+let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
+
+
+if (produtoA.internacional = true) 
+{
+    produtoA_valor2 = produtoA.valor * 1.2
+console.log("camiseta: ", produtoA_valor2 );
+
+}else {
+
+    produtoA_valor2 = produtoA.valor * 1
+console.log("camiseta: ", produtoA_valor2 );
+
+}
+
+if (produtoB.internacional = true) 
+{
+    produtoB_valor2 = produtoB.valor * 1.2
+console.log("perfume: ", produtoB_valor2 );
+
+}else {
+
+    produtoB_valor2 = produtoB.valor * 1
+console.log("perfume: ", produtoB_valor2 );
+
+}
+
+if (produtoC.internacional = true) 
+{
+    produtoC_valor2 = produtoC.valor * 1.2
+console.log("sandália: ", produtoC_valor2 );
+
+}else {
+
+    produtoC_valor2 = produtoC.valor * 1
+console.log("sandália: ", produtoC_valor2 );
+
+}
+
